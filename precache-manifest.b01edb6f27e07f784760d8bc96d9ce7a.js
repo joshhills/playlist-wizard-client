a17de9d49bf1a52e1f36ce21999742a8ec4cdb46
Josh Hills <joshhills@Joshs-MacBook-Pro.local>
2020-07-10 23:49:13 +0100

    Updates

---
+++ precache-manifest.b01edb6f27e07f784760d8bc96d9ce7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcc9dd7812c3265bb00eefbe8422018",
+    "url": "/playlist-wizard-client/index.html"
+  },
+  {
+    "revision": "bc50bfd643255f060176",
+    "url": "/playlist-wizard-client/static/css/main.6942973f.chunk.css"
+  },
+  {
+    "revision": "1af60fb766105df618ba",
+    "url": "/playlist-wizard-client/static/js/2.48b51c8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/playlist-wizard-client/static/js/2.48b51c8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc50bfd643255f060176",
+    "url": "/playlist-wizard-client/static/js/main.355e3f4a.chunk.js"
+  },
+  {
+    "revision": "a32f0af81d2619fb691f",
+    "url": "/playlist-wizard-client/static/js/runtime-main.8d48954e.js"
+  }
+]);
